4ed9df8703f5f19e828d5921060cc7f29bc68ec9
Amadeus <amadeusarkhammorningstar@gmail.com>
2020-10-02 12:12:25 +0600

    Everything is ready except edit contact

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import contactList from './data.js';
 import ContacTable from './components/ContacTable';
 import AddContact from './components/AddContact';
-import EditContact from './components/EditContact.jsx';
 
 const App =()=>{
   const [contacts, setContacts] = useState(contactList);
@@ -13,14 +12,7 @@ const App =()=>{
   const deleteContact = (id) =>{
     setContacts(contacts.filter(contact => contact.id!==id));
   }
-
-  const [edit, setEditing] = useState(false);
-  const newContact = {id:null, name:'', age:'', city:''};
-  const [currentcontact, currentContact] = useState(newContact);
   
-  const updateContact = (newContact) =>{
-    setContacts(contacts.map(contact=>(contact.id === currentContact.id ? newContact : contact)))
-  }
   return(
     <div className="container">
       <h1>Test task</h1>
@@ -29,8 +21,7 @@ const App =()=>{
           {edit? (
             <div>
               <h2>Update contact</h2>
-                <EditContact
-                />
+                
             </div>
           ):(
             <div>
